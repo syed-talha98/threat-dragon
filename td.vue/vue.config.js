520e0750ed60b703997e2528c73425c168e60ba4
Imran Rehman <imran.ismail.rehman@gmail.com>
2024-11-29 17:59:53 +0000

    Add WebSocket configuration for secure proxying

--- td.vue/vue.config.js
+++ td.vue/vue.config.js
@@ -5,18 +5,25 @@ require('dotenv').config({ path: process.env.ENV_FILE || path.resolve(__dirname,
 const serverApiProtocol = process.env.SERVER_API_PROTOCOL || 'http';
 const serverApiPort = process.env.PORT || '3000';
 console.log('Server API protocol: ' + serverApiProtocol + ' and port: ' + serverApiPort);
-
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/public' : '/',
     productionSourceMap: false,
     devServer: {
+        client: {
+            webSocketURL: {
+                protocol: 'wss', // Use secure WebSocket protocol
+                hostname: 'td.devflovv.com', // Replace with your domain
+                port: 443, // HTTPS port
+            },
+        },
         proxy: {
             '^/api': {
-                target: serverApiProtocol + '://localhost:' + serverApiPort,
-                ws: true,
-                changeOrigin: true
-            }
-        }
+                target: `${serverApiProtocol}://localhost:${serverApiPort}`, // Backend server
+                ws: true, // Proxy WebSocket connections
+                changeOrigin: true,
+            },
+        },
+        allowedHosts: "all",
     },
     lintOnSave: false,
     pluginOptions: {
